46f1b8071ac7623c958d565fb42e1f35e233c460
Suraj Adhikari <heartbreakkidhbk97@gmail.com>
2019-10-13 10:33:14 +0545

    fixed Navbar

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assests like main.js,main.css file
+  app.use(express.static("client/build"));
+  //if any get req come from some routes that we haven't define,files & we donot understand what its looking for than looki into client/build dir
+
+  //If abouve statement fail than this runs Express will serveup index.html file
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
